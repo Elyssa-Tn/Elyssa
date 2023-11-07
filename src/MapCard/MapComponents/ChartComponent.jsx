fa1fdc70ca4196fa1b04363c7790ae4a8edd2a99
MABGuiza <benguizameda@proton.me>
2023-11-07 17:52:59 +0100

    Small size adjustments

--- src/MapCard/MapComponents/ChartComponent.jsx
+++ src/MapCard/MapComponents/ChartComponent.jsx
@@ -8,14 +8,13 @@ function ChartComponent({ data, bounds }) {
 
   const names = Object.values(bounds[level]).map((item) => item.name);
   const values = Object.values(data[level]["prc"]).slice(0, -1);
-  console.log(values);
 
   return (
     <BarChart
       xAxis={[{ id: "values", data: names, scaleType: "band" }]}
       series={[{ data: values }]}
-      width={500}
-      height={300}
+      width={450}
+      height={600}
     />
   );
 }
