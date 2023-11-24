1a2656bbd98c61098839dfaefd2a242978bf65d5
MABGuiza <benguizameda@proton.me>
2023-11-24 17:01:11 +0100

    Progress.

--- src/MapCard/MapComponents/ChartComponent.jsx
+++ src/MapCard/MapComponents/ChartComponent.jsx
@@ -9,9 +9,11 @@ import {
 } from "chart.js";
 import annotationPlugin from "chartjs-plugin-annotation";
 import { useDispatch, useSelector } from "react-redux";
-import { Box } from "@mui/joy";
+import { Box, Button, ButtonGroup, Radio, Switch } from "@mui/joy";
 import { useEffect, useState } from "react";
 import { setHover } from "../../reducers/interfaceReducer";
+import EqualizerOutlinedIcon from "@mui/icons-material/EqualizerOutlined";
+import ExploreIcon from "@mui/icons-material/Explore";
 
 const ChartComponent = ({ data, bounds }) => {
   Chart.register(
@@ -28,9 +30,8 @@ const ChartComponent = ({ data, bounds }) => {
   const hover = useSelector((state) => state.interface.hover);
   const dispatch = useDispatch();
 
-  console.log(hover);
-
   const [datasets, setDatasets] = useState(null);
+  const [displayAverage, setDisplayAverage] = useState(false);
 
   useEffect(() => {
     const combinedObject = [];
@@ -54,63 +55,99 @@ const ChartComponent = ({ data, bounds }) => {
     setDatasets(combinedObject);
   }, [bounds, data, currentTarget, level]);
 
-  const average = data[level]["prc"]["Total"];
+  const average = data["gouvernorat"]["prc"]["Total"];
 
   return (
-    <Box height={500} width={400}>
-      <Bar
-        data={{
-          datasets: [{ data: datasets }],
+    <Box>
+      <ButtonGroup>
+        <Button
+          variant="soft"
+          endDecorator={
+            <Switch
+              checked={displayAverage}
+              onChange={() => setDisplayAverage(event.target.checked)}
+            />
+          }
+        >
+          Moyenne generale
+        </Button>
+      </ButtonGroup>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row-reverse",
         }}
-        options={{
-          parsing: {
-            xAxisKey: "name",
-            yAxisKey: "value",
-          },
-          onHover: (event, elements) => {
-            if (elements.length) {
-              dispatch(setHover(datasets[elements[0].index]));
-            }
-          },
-          maintainAspectRatio: false,
-          responsive: true,
-          elements: {
-            bar: {
-              backgroundColor: "blue",
-              hoverBackgroundColor: "red",
-              hoverBorderWidth: "1",
-            },
-          },
-          plugins: {
-            annotation: {
-              annotations: [
-                {
-                  type: "line",
-                  value: average,
-                  yMin: average,
-                  yMax: average,
-                  borderColor: "rgb(75, 192, 192)",
-                  borderWidth: 2,
-                  label: {
-                    enabled: false,
-                    content: "Test label",
+      >
+        <ButtonGroup
+          orientation="vertical"
+          size="sm"
+          sx={{ padding: "0.25rem" }}
+        >
+          <Button>
+            <EqualizerOutlinedIcon />
+          </Button>
+          <Button>
+            <ExploreIcon />
+          </Button>
+        </ButtonGroup>
+        <Box height={500} width={400}>
+          <Bar
+            data={{
+              datasets: [{ data: datasets }],
+            }}
+            options={{
+              parsing: {
+                xAxisKey: "name",
+                yAxisKey: "value",
+              },
+              onHover: (event, elements) => {
+                if (elements.length) {
+                  dispatch(setHover(datasets[elements[0].index]));
+                }
+              },
+              maintainAspectRatio: false,
+              responsive: true,
+              elements: {
+                bar: {
+                  backgroundColor: "blue",
+                  hoverBackgroundColor: "red",
+                  hoverBorderWidth: "1",
+                },
+              },
+              plugins: {
+                annotation: {
+                  annotations: displayAverage
+                    ? [
+                        {
+                          type: "line",
+                          value: average,
+                          yMin: average,
+                          yMax: average,
+                          borderColor: "rgb(75, 192, 192)",
+                          borderWidth: 2,
+                          label: {
+                            enabled: false,
+                            content: "Test label",
+                          },
+                        },
+                      ]
+                    : null,
+                },
+                tooltip: {
+                  callbacks: {},
+                  padding: 12,
+                  titleFont: {
+                    size: 22,
+                  },
+                  bodyFont: {
+                    size: 18,
                   },
                 },
-              ],
-            },
-            tooltip: {
-              callbacks: {},
-              padding: 12,
-              titleFont: {
-                size: 22,
               },
-              bodyFont: {
-                size: 18,
-              },
-            },
-          },
-        }}
-      />
+            }}
+          />
+        </Box>
+      </Box>
     </Box>
   );
 };
